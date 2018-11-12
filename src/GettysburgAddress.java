134c09d464c1443d8063d887281da8960602c6ce
pawasthi20 <43074073+pawasthi20@users.noreply.github.com>
2018-11-11 23:26:24 -0500

    Add files via upload

--- src/GettysburgAddress.java
+++ src/GettysburgAddress.java
@@ -4,34 +4,53 @@
  *
  */
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
-
 
 public class GettysburgAddress 
 {
-	public static void main(String[] args)
+	public static void main(String[] args) throws FileNotFoundException
 	{
-		Scanner file = new Scanner(new File(filename));
-		ArrayList words = new ArrayList();
-
+		//Creating an Array List of words 
+		Scanner file = new Scanner(new File("/Users/parth/git/GettysburgAddress/src/GettysburgAddress.txt")).useDelimiter(" |\\n|-");
+		
+		//Looked up useDelimiter method to delete special characters
+		ArrayList<String> words = new ArrayList<String>();
+		
+		//Declaring Variables
+		String word;
+		String longestWord = null;
+		int wordLength;
+		int longestWordlength = 0;
+		int totalWordlength = 0;
+		int numWords;
+		int averageWordlength = 0;
+				
+		//Adding each word into the ArrayList and calculating longest word and average word length
 		while (file.hasNext())
 		{	
-			int max = ints.get(0);
-			for(int words: ints)
-			{
-				if(words>max)
-					max = words;
-			}
-		}
-		
-		while (file.hasNext()) 
-		{
 			word = file.next();
 			words.add(word);
-			for(i)
+			wordLength = word.length();
+			if(wordLength > longestWordlength)
 			{
-				String longestWord = 
+				longestWordlength = wordLength;
+				longestWord = word;
 			}
-		} 
+			totalWordlength += wordLength;
+		}
+		numWords = words.size();
+		
+		//Making sure if file does not have words, program does not crash
+		if (numWords > 0)
+		{
+			averageWordlength = totalWordlength/numWords;
+		}
+		
+		//Printing
+		System.out.println("The longest word in the Gettysburg Address is: " + longestWord);
+		System.out.println("The average word length in the Gettysburg Address is: " + averageWordlength);
+		file.close();
 	}	
 }
